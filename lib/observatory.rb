117b81f4323b591daebe2ebd7ace61c99f55aa13
Arjan van der Gaag <info@agwebdesign.nl>
2011-05-01 21:10:03 +0200

    Autoload components

--- lib/observatory.rb
+++ lib/observatory.rb
@@ -1,7 +1,6 @@
 module Observatory
-  # Your code goes here...
-end
-
-%w{dispatcher event observable observer}.each do |f|
-  require File.join(File.dirname(__FILE__), 'observatory', f)
+  autoload :Dispatcher, 'observatory/dispatcher'
+  autoload :Event,      'observatory/event'
+  autoload :Observable, 'observatory/observable'
+  autoload :Observer,   'observatory/observer'
 end
